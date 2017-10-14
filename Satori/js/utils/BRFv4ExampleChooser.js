0789a0e416879632d4038afe7250997ea0d243f7
nkkumawat <nk0kumawat@gmail.com>
2017-10-14 18:13:23 +0530

    ui changed

--- Satori/js/utils/BRFv4ExampleChooser.js
+++ Satori/js/utils/BRFv4ExampleChooser.js
@@ -4,9 +4,9 @@
 	if(typeof QuickSettings === "undefined") return;
 
 	var urlMap 	= {
-		"Candide overlay":		"js/examples/candide_overlay.js",
+		"Candide":		"js/examples/candide_overlay.js",
 		// "Smile detection":		"js/examples/smile_detection.js",
-		"png - mask overlay":	"js/examples/png_mask_overlay.js",
+		"Face Draw":	"js/examples/png_mask_overlay.js",
 		//"3D example":			"js/examples/ThreeJS_example.js",
 		//"Face texture":			["assets/brfv4_face_textures.js", "js/examples/face_texture_overlay.js"]
 	};
@@ -37,17 +37,41 @@
 		}
 	}
 
+	var mode_change = $('.mode-changer');
+
+	mode_change.click(function () {
+		var value = this.name;
+		var index = this.about;
+        if(_isFirstSelect) return;
+
+        var url = urlMap[value];
+
+        if(url) {
+            if(typeof url === "string") {
+                brfv4Example.loader.loadExample([url], onExampleLoaded);
+            } else {
+                brfv4Example.loader.loadExample(url, onExampleLoaded);
+            }
+        } else {
+            if(data.index >= 0) {
+                brfv4Example.gui.exampleChooser.setValuesFromJSON({ "_example": index + 1});
+            }
+        }
+    });
+
+
+
 	if(!brfv4Example.gui.exampleChooser) {
 
-		QuickSettings.useExtStyleSheet();
-
-		brfv4Example.gui.exampleChooser = QuickSettings.create(
-			2, 2, "Example Chooser", brfv4Example.dom.createDiv("_settingsRight"))
-			.setWidth(250)
-			// .addHTML("Switch between examples", "Which example do you want to try? Use the drop down to choose another example.").hideTitle("Switch between examples")
-			.addDropDown("_example", labels, onExampleChosen)
-			.hideTitle("_example")
-			.setValuesFromJSON({ "_example": 0	}); // "basic - face tracking - track single face"
+		// QuickSettings.useExtStyleSheet();
+        //
+		// brfv4Example.gui.exampleChooser = QuickSettings.create(
+		// 	2, 2, "Example Chooser", brfv4Example.dom.createDiv("_settingsRight"))
+		// 	.setWidth(250)
+		// 	// .addHTML("Switch between examples", "Which example do you want to try? Use the drop down to choose another example.").hideTitle("Switch between examples")
+		// 	.addDropDown("_example", labels, onExampleChosen)
+		// 	.hideTitle("_example")
+		// 	.setValuesFromJSON({ "_example": 0	}); // "basic - face tracking - track single face"
 
 		_isFirstSelect = false;
 	}
